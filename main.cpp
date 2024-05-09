b885afbe05209e8def6ca14b3fd7ec2b6f6808af
takuto-0130 <k023g0005@g.neec.ac.jp>
2024-05-09 19:44:13 +0900

    a

--- main.cpp
+++ main.cpp
@@ -5,6 +5,26 @@
 const int kWindowWidth = 1280;
 const int kWindowHeight = 720;
 
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
+	const float kGridHalfWidth = 2.0f;
+	const uint32_t kSubdivision = 10;
+	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision);
+
+	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex) {
+		Vector3 start = { -kGridHalfWidth * float(kGridEvery), 0, -kGridHalfWidth * float(kGridEvery + xIndex) };
+		Vector3 end = { kGridHalfWidth * float(kGridEvery), 0, start.z };
+		Vector3 ndcStartVertex = Transform(start, viewProjectionMatrix);
+		Vector3 ndcEndVertex = Transform(end, viewProjectionMatrix);
+		Vector3 screenStart = Transform(ndcStartVertex, viewportMatrix);
+		Vector3 screenEnd = Transform(ndcEndVertex, viewportMatrix);
+		if (xIndex == kSubdivision / 2) {
+			Novice::DrawLine(int(screenStart.x), int(screenStart.z), int(screenEnd.x), int(screenEnd.z), BLACK);
+		}
+		else {
+			Novice::DrawLine(int(screenStart.x), int(screenStart.z), int(screenEnd.x), int(screenEnd.z), WHITE);
+		}
+	}
+}
 
 
 const char kWindowTitle[] = "LC1A_27_ヤマグチ_タクト_タイトル";
@@ -15,30 +35,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Vector3 v1{ 1.2f,-3.9f,2.5f };
-	Vector3 v2{ 2.8f,0.4f,-1.3f };
-	Vector3 cross = Cross(v1, v2);
-
-	Vector3 rotate{};
-	Vector3 translate{};
-	const Vector3 kLocalVertices[3] = {
-		{0.0f,0.2f,0.0f},
-		{0.2f, -0.2f,0.0f},
-		{-0.2f,-0.2f,0.0f}
-	};
-	Vector3 cametaPosition{ 0,0,-1.0f };
-
-	Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
-	Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, cametaPosition);
-	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
-	Matrix4x4 projectionMatrix = MakePrespectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
-	Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-	Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
-	Vector3 screenVertices[3];
-	for (uint32_t i = 0; i < 3; ++i) {
-		Vector3 ndcVertex = Transform(kLocalVertices[i], worldViewProjectionMatrix);
-		screenVertices[i] = Transform(ndcVertex, viewportMatrix);
-	}
+	
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -57,25 +54,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		if (keys[DIK_A]) {
-			rotate.y += 0.03f;
-		}
-		if (keys[DIK_D]) {
-			rotate.y -= 0.03f;
-		}
-		if (keys[DIK_W]) {
-			translate.z += 0.03f;
-		}
-		if (keys[DIK_S]) {
-			translate.z -= 0.03f;
-		}
-
-		worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
-		worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-		for (uint32_t i = 0; i < 3; ++i) {
-			Vector3 ndcVertex = Transform(kLocalVertices[i], worldViewProjectionMatrix);
-			screenVertices[i] = Transform(ndcVertex, viewportMatrix);
-		}
 
 		///
 		/// ↑更新処理ここまで
@@ -85,10 +63,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		
-		Novice::ScreenPrintf(0, 0, "%.02f %.02f %.02f Cross", cross.x, cross.y, cross.z);
-		Novice::DrawTriangle(int(screenVertices[0].x), int(screenVertices[0].y), int(screenVertices[1].x), int(screenVertices[1].y),
-			int(screenVertices[2].x), int(screenVertices[2].y), RED, kFillModeSolid);
-
 
 		///
 		/// ↑描画処理ここまで
