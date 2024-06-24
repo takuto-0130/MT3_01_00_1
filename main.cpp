86d879a235fd438ac369178a1c720bf1b95ea735
takuto-0130 <k023g0005@g.neec.ac.jp>
2024-06-24 12:47:07 +0900

    MT3_02_10_応用課題

--- main.cpp
+++ main.cpp
@@ -229,6 +229,102 @@ bool IsCollisionOBBSegment(const OBB& obb, const Segment& segment) {
 	return IsCollisionAABBSegment(aabbOBBLocal, localSegment);
 }
 
+void OBBVertex(const OBB& obb, Vector3* vertixces) {
+	Vector3 sizeX = (obb.oriientations[0]) * obb.size.x;
+	Vector3 sizeY = (obb.oriientations[1]) * obb.size.y;
+	Vector3 sizeZ = (obb.oriientations[2]) * obb.size.z;
+	vertixces[0] = -sizeX - sizeY - sizeZ + obb.center;
+	vertixces[1] = -sizeX + sizeY - sizeZ + obb.center;
+	vertixces[2] = sizeX - sizeY - sizeZ + obb.center;
+	vertixces[3] = sizeX + sizeY - sizeZ + obb.center;
+	vertixces[4] = -sizeX - sizeY + sizeZ + obb.center;
+	vertixces[5] = -sizeX + sizeY + sizeZ + obb.center;
+	vertixces[6] = sizeX - sizeY + sizeZ + obb.center;
+	vertixces[7] = sizeX + sizeY + sizeZ + obb.center;
+}
+
+/// <summary>
+/// 分離軸があるかどうか
+/// </summary>
+/// <param name="normal"> 正規化法線ベクトル </param>
+/// <param name="vertixces1"> OBB1の頂点 </param>
+/// <param name="vertixces2"> OBB2の頂点 </param>
+/// <returns></returns>
+bool isHST(const Vector3& normal, const  Vector3* vertixces1, const  Vector3* vertixces2) {
+	float t[8];
+	for (size_t i = 0; i < 8; i++) {
+		t[i] = Dot(normal, vertixces1[i]);
+	}
+	float minT1 = t[0], maxT1 = t[0];
+	for (size_t i = 1; i < 8; i++) {
+		minT1 = (std::min)(t[i], minT1);
+		maxT1 = (std::max)(t[i], maxT1);
+	}
+	float L1 = maxT1 - minT1;
+
+	for (size_t i = 0; i < 8; i++) {
+		t[i] = Dot(normal, vertixces2[i]);
+	}
+	float minT2 = t[0], maxT2 = t[0];
+	for (size_t i = 1; i < 8; i++) {
+		minT2 = (std::min)(t[i], minT2);
+		maxT2 = (std::max)(t[i], maxT2);
+	}
+	float L2 = maxT2 - minT2;
+	float sumSpan = L1 + L2;
+	float longSpan = (std::max)(maxT1, maxT2) - (std::min)(minT1, minT2);
+	if (sumSpan < longSpan) {
+		return true;
+	}
+
+
+	return false;
+}
+
+bool IsCollisionOBBOBB(const OBB& obb1, const OBB& obb2) {
+	Vector3 obb1Vertices[8]{};
+	OBBVertex(obb1, obb1Vertices);
+	Vector3 obb2Vertices[8]{};
+	OBBVertex(obb2, obb2Vertices);
+	for (size_t i = 0; i < 3; i++) {
+		if (isHST(obb1.oriientations[i], obb1Vertices, obb2Vertices)) {
+			return false;
+		}
+		if (isHST(obb2.oriientations[i], obb1Vertices, obb2Vertices)) {
+			return false;
+		}
+	}
+	Vector3 cross{};
+	for (size_t i = 0; i < 3; i++) {
+		for (size_t j = 0; j < 3; j++) {
+			cross = Cross(obb1.oriientations[i], obb2.oriientations[j]);
+			cross = Normalize(cross);
+			if (isHST(cross, obb1Vertices, obb2Vertices)) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void OBBRotation(const Vector3& rotate, OBB& obb) {
+	Matrix4x4 OBBrotateMatrix;
+
+	OBBrotateMatrix =
+		Multiply(MakeRotateXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateZMatrix(rotate.z)));
+	obb.oriientations[0].x = OBBrotateMatrix.m[0][0];
+	obb.oriientations[0].y = OBBrotateMatrix.m[0][1];
+	obb.oriientations[0].z = OBBrotateMatrix.m[0][2];
+
+	obb.oriientations[1].x = OBBrotateMatrix.m[1][0];
+	obb.oriientations[1].y = OBBrotateMatrix.m[1][1];
+	obb.oriientations[1].z = OBBrotateMatrix.m[1][2];
+
+	obb.oriientations[2].x = OBBrotateMatrix.m[2][0];
+	obb.oriientations[2].y = OBBrotateMatrix.m[2][1];
+	obb.oriientations[2].z = OBBrotateMatrix.m[2][2];
+}
+
 
 const char kWindowTitle[] = "LE2A_18_ヤマグチ_タクト_タイトル";
 
@@ -258,19 +354,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Multiply(MakeRotateXMatrix(OBBrotate.x), Multiply(MakeRotateYMatrix(OBBrotate.y), MakeRotateZMatrix(OBBrotate.z)));
 
 	OBB obb{
-		.center {-1.0f,0.0f,0.0f},
+		.center {0.0f,0.0f,0.0f},
 		.oriientations{
 			{1,0,0},
 			{0,1,0},
 			{0,0,1}
 			},
-		.size{0.5f,0.5f,0.5f}
+		.size{0.83f,0.26f,0.24f}
 	};
 
-	Segment segment{
-		.origine{-0.8f, -0.3f,0.0f},
-		.diff{0.5f,0.5f,0.5f}
+	Vector3 OBBrotate2{ 0,0,0 };
+	Matrix4x4 OBBrotateMatrix2 =
+		Multiply(MakeRotateXMatrix(OBBrotate2.x), Multiply(MakeRotateYMatrix(OBBrotate2.y), MakeRotateZMatrix(OBBrotate2.z)));
+
+	OBB obb2{
+		.center {0.9f,0.66f,0.78f},
+		.oriientations{
+			{1,0,0},
+			{0,1,0},
+			{0,0,1}
+			},
+		.size{0.5f,0.37f,0.5f}
 	};
+
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -290,31 +396,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		ImGui::Begin("Window");
-		ImGui::DragFloat3("obb.center", &obb.center.x, 0.01f);
-		ImGui::DragFloat3("obb.size", &obb.size.x, 0.01f);
-		ImGui::DragFloat3("OBBrotate", &OBBrotate.x, 0.01f);
-		ImGui::DragFloat3("segment.origine", &segment.origine.x, 0.01f);
-		ImGui::DragFloat3("segment.diff", &segment.diff.x, 0.01f);
-		ImGui::DragFloat2("WorldRotate", &rotate.x, 0.01f);
+		ImGui::DragFloat3("obb1.center", &obb.center.x, 0.01f);
+		ImGui::DragFloat3("obb1.size", &obb.size.x, 0.01f);
+		ImGui::DragFloat3("obb1rotate", &OBBrotate.x, 0.01f);
+		ImGui::DragFloat3("obb2.center", &obb2.center.x, 0.01f);
+		ImGui::DragFloat3("obb2.size", &obb2.size.x, 0.01f);
+		ImGui::DragFloat3("obb2rotate", &OBBrotate2.x, 0.01f);
+		ImGui::DragFloat2("cameraWorldRotate", &rotate.x, 0.01f);
 		ImGui::End();
 
 		obb.size.x = (std::max)(obb.size.x, 0.02f);
 		obb.size.y = (std::max)(obb.size.y, 0.02f);
 		obb.size.z = (std::max)(obb.size.z, 0.02f);
 
-		OBBrotateMatrix =
-			Multiply(MakeRotateXMatrix(OBBrotate.x), Multiply(MakeRotateYMatrix(OBBrotate.y), MakeRotateZMatrix(OBBrotate.z)));
-		obb.oriientations[0].x = OBBrotateMatrix.m[0][0];
-		obb.oriientations[0].y = OBBrotateMatrix.m[0][1];
-		obb.oriientations[0].z = OBBrotateMatrix.m[0][2];
+		obb2.size.x = (std::max)(obb2.size.x, 0.02f);
+		obb2.size.y = (std::max)(obb2.size.y, 0.02f);
+		obb2.size.z = (std::max)(obb2.size.z, 0.02f);
 
-		obb.oriientations[1].x = OBBrotateMatrix.m[1][0];
-		obb.oriientations[1].y = OBBrotateMatrix.m[1][1];
-		obb.oriientations[1].z = OBBrotateMatrix.m[1][2];
-
-		obb.oriientations[2].x = OBBrotateMatrix.m[2][0];
-		obb.oriientations[2].y = OBBrotateMatrix.m[2][1];
-		obb.oriientations[2].z = OBBrotateMatrix.m[2][2];
+		OBBRotation(OBBrotate, obb);
+		OBBRotation(OBBrotate2, obb2);
 
 		cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cametaPosition);
 		cameraMatrix = Multiply(cameraMatrix, MakeRotateXMatrix(rotate.x));
@@ -323,7 +423,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
 
-		if (IsCollisionOBBSegment(obb, segment)) {
+		if (IsCollisionOBBOBB(obb, obb2)) {
 			color = RED;
 		}
 		else {
@@ -340,8 +440,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		DrawGrid(worldViewProjectionMatrix, viewportMatrix);
-		DrawSegment(segment, worldViewProjectionMatrix, viewportMatrix, WHITE);
 		DrawOBB(obb, worldViewProjectionMatrix, viewportMatrix, color);
+		DrawOBB(obb2, worldViewProjectionMatrix, viewportMatrix, WHITE);
 
 
 		///
