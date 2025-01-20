f9aafc429905cd78cb7f994440133370c9b3f7a2
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-01-20 12:51:11 +0900

    01_05_確認課題

--- main.cpp
+++ main.cpp
@@ -173,11 +173,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		
-		Novice::ScreenPrintf(0, 0, "%5.2f %5.2f %5.2f %5.2f   : interpolate0", interpolate0.x, interpolate0.y, interpolate0.z, interpolate0.w);
-		Novice::ScreenPrintf(0, 20, "%5.2f %5.2f %5.2f %5.2f   : interpolate1", interpolate1.x, interpolate1.y, interpolate1.z, interpolate1.w);
-		Novice::ScreenPrintf(0, 40, "%5.2f %5.2f %5.2f %5.2f   : interpolate2", interpolate2.x, interpolate2.y, interpolate2.z, interpolate2.w);
-		Novice::ScreenPrintf(0, 60, "%5.2f %5.2f %5.2f %5.2f   : interpolate3", interpolate3.x, interpolate3.y, interpolate3.z, interpolate3.w);
-		Novice::ScreenPrintf(0, 80, "%5.2f %5.2f %5.2f %5.2f   : interpolate4", interpolate4.x, interpolate4.y, interpolate4.z, interpolate4.w);
+		Novice::ScreenPrintf(0, 0, "%5.2f %5.2f %5.2f %5.2f   : interpolate0, Slerp(q0, q1, 0.0f)", interpolate0.x, interpolate0.y, interpolate0.z, interpolate0.w);
+		Novice::ScreenPrintf(0, 20, "%5.2f %5.2f %5.2f %5.2f   : interpolate1, Slerp(q0, q1, 0.3f)", interpolate1.x, interpolate1.y, interpolate1.z, interpolate1.w);
+		Novice::ScreenPrintf(0, 40, "%5.2f %5.2f %5.2f %5.2f   : interpolate2, Slerp(q0, q1, 0.5f)", interpolate2.x, interpolate2.y, interpolate2.z, interpolate2.w);
+		Novice::ScreenPrintf(0, 60, "%5.2f %5.2f %5.2f %5.2f   : interpolate3, Slerp(q0, q1, 0.7f)", interpolate3.x, interpolate3.y, interpolate3.z, interpolate3.w);
+		Novice::ScreenPrintf(0, 80, "%5.2f %5.2f %5.2f %5.2f   : interpolate4, Slerp(q0, q1, 1.0f)", interpolate4.x, interpolate4.y, interpolate4.z, interpolate4.w);
 		/*Novice::ScreenPrintf(0, 0, "%5.2f %5.2f %5.2f %5.2f   : Identity", identity.x, identity.y, identity.z, identity.w);
 		Novice::ScreenPrintf(0, 20, "%5.2f %5.2f %5.2f %5.2f   : Conjugate", conj.x, conj.y, conj.z, conj.w);
 		Novice::ScreenPrintf(0, 40, "%5.2f %5.2f %5.2f %5.2f   : Inverse", inv.x, inv.y, inv.z, inv.w);*/
